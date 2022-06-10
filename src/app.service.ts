6b8371ea920d3d239b6806b0101bc9e891b15c58
Karim Salem <karimsalem25@gmail.com>
2022-06-10 22:11:51 +0200

    Updating payment

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+require('dotenv').config();
+
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async payment(amount: number) {
+    const stripe = require('stripe')(
+      process.env.STRIPE_SECRET_KEY1,
+    );
+
+    const paymentIntent = await stripe.charges.create(
+      {
+        amount: amount,
+        currency: 'eur',
+        source: 'tok_amex'
+      },
+      {
+        stripeAccount: process.env.STRIPE_ACCOUNT_ID1,
+      },
+    );
+    return paymentIntent;
   }
 }
